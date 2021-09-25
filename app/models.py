f0f5bf928b92c690ce7a1f86caa1f0fa6358bdb8
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-25 03:15:37 +0300

    add user class

--- app/models.py
+++ app/models.py
@@ -1,2 +1,16 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    subscription = db.Column(db.Boolean)
+    date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
