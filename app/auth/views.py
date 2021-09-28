915908a0450dac9874f7c918fd67b514eb3b5228
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-28 12:29:14 +0300

    make it better

--- app/auth/views.py
+++ app/auth/views.py
@@ -20,7 +20,7 @@ def login():
         flash('Invalid username or Password')
 
     title = "Blog login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+    return render_template('auth/login.html',login_form=login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -34,7 +34,7 @@ def register():
 
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+    return render_template('auth/register.html',registration_form=form)
 
 
 @auth.route('/logout')
